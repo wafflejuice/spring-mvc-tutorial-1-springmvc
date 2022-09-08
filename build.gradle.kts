72b2aef16a7522479666a06e99873dbaed3919f8
wafflejuice <wafflejuice96@gmail.com>
2022-09-08 23:28:27 +0900

    add spring web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
