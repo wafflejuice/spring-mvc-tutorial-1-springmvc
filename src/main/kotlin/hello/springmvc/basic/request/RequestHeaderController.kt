f32e010e33ed01d20774eaaf42f342a6d30915d4
wafflejuice <wafflejuice96@gmail.com>
2022-09-14 23:03:22 +0900

    HTTP 요청 - 기본, 헤더 조회

---
+++ src/main/kotlin/hello/springmvc/basic/request/RequestHeaderController.kt
@@ -0,0 +1,39 @@
+package hello.springmvc.basic.request
+
+import Slf4j
+import Slf4j.Companion.logger
+import org.springframework.http.HttpMethod
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Slf4j
+@RestController
+class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    fun headers(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        httpMethod: HttpMethod,
+        local: Locale,
+        @RequestHeader headerMap: MultiValueMap<String, String>,
+        @RequestHeader("host") host: String,
+        @CookieValue(value = "myCookie", required = false) cookie: String?
+    ): String {
+        logger.info("request=${request}")
+        logger.info("response=${response}")
+        logger.info("httpMethod=${httpMethod}")
+        logger.info("local=${local}")
+        logger.info("headerMap=${headerMap}")
+        logger.info("host=${host}")
+        logger.info("cookie=${cookie}")
+
+        return "ok"
+    }
+}
