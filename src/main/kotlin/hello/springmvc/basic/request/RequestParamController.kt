eae9af22c1bc9ebc4b5a95b6eb70ab7f8476f9dd
wafflejuice <wafflejuice96@gmail.com>
2022-09-15 23:59:11 +0900

    HTTP 요청 파라미터 - @RequestParam

--- src/main/kotlin/hello/springmvc/basic/request/RequestParamController.kt
+++ src/main/kotlin/hello/springmvc/basic/request/RequestParamController.kt
@@ -4,6 +4,8 @@ import Slf4j
 import Slf4j.Companion.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -21,5 +23,70 @@ class RequestParamController {
         logger.info("username=${username}, age=${age}")
 
         response.writer.write("ok")
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    fun requestParamV2(
+        @RequestParam("username") memberName: String,
+        @RequestParam("age") memberAge: Int,
+    ): String {
+        logger.info("username=${memberName}, age=${memberAge}")
+
+        return "ok"
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    fun requestParamV3(
+        @RequestParam username: String,
+        @RequestParam age: Int,
+    ): String {
+        logger.info("username=${username}, age=${age}")
+
+        return "ok"
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    fun requestParamV4(
+        username: String,
+        age: Int,
+    ): String {
+        logger.info("username=${username}, age=${age}")
+
+        return "ok"
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    fun requestParamRequired(
+        @RequestParam(required = true) username: String,
+        @RequestParam(required = false) age: Int?,
+    ): String {
+        logger.info("username=${username}, age=${age}")
+
+        return "ok"
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    fun requestParamDefault(
+        @RequestParam(defaultValue = "guest") username: String,
+        @RequestParam(defaultValue = "-1") age: Int?,
+    ): String {
+        logger.info("username=${username}, age=${age}")
+
+        return "ok"
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    fun requestParamMap(
+        @RequestParam paramMap: Map<String, Any>,
+    ): String {
+        logger.info("username=${paramMap["username"]}, age=${paramMap["age"]}")
+
+        return "ok"
     }
 }
